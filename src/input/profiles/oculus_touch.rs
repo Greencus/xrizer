2b9ef29886c5397d52dda191b3cd415d879b6b2e
Greencus2 <greenmarcus.g@gmail.com>
2026-07-25 15:42:38 -0400

    Add translation for left thumbrest to menu path

--- src/input/profiles/oculus_touch.rs
+++ src/input/profiles/oculus_touch.rs
@@ -61,6 +61,17 @@ impl InteractionProfile for OculusTouch {
     }
     fn translate_path(path: DynInputPath) -> Option<DynInputPath> {
         match path {
+             DynInputPath {
+            hand: crate::openxr_data::Hand::Left,
+            subpath: DynSubpath::Thumbrest,
+            component: Some(DynComponent::Touch),
+        } if crate::config::remap_left_thumbrest_to_menu() => {
+            Some(DynInputPath {
+                hand: crate::openxr_data::Hand::Left,
+                subpath: DynSubpath::Menu,
+                component: Some(DynComponent::Click),
+            })
+        }
             p @ DynInputPath {
                 subpath: DynSubpath::Squeeze | DynSubpath::Trigger,
                 component: Some(DynComponent::Click),
